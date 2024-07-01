9b97d449039a21e9df00650f8ba55e526aedaf95
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-07-01 09:45:21 +0100

    minor UI fixes

--- src/components/sections/services/BottomSection.tsx
+++ src/components/sections/services/BottomSection.tsx
@@ -12,7 +12,7 @@ const BottomSection = () => {
 
   return (
     <div className="lg:w-[90vw] mx-auto mt-[10vh]">
-      <div className="bg-[url('/assets/services/img_four.png')] bg-no-repeat bg-cover relative lg:min-h-[70vh] min-h-[100vh] text-[#fff] lg:rounded-t-2xl">
+      <div className="bg-[url('/assets/services/img_four.png')] bg-no-repeat bg-cover relative lg:min-h-[70vh] min-h-[130vh] text-[#fff] lg:rounded-t-2xl">
         <div className="bg-[#000] z-10 absolute w-[100%] h-[100%] bg-opacity-60 lg:py-[10vh] py-10 lg:px-10 px-4 lg:rounded-t-2xl">
           <div className="text-[15px] flex items-center space-x-3">
             <div className="h-1 w-6 bg-[#3e4784]"></div>
